4a8d5c2a73fb340595c4535d5b26706b3b42e69b
Samir Jafferali <me@samir.id>
2025-07-15 10:28:20 -0700

    fix

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,3 +1,8 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 // Mock import.meta.env
 global.importMetaEnv = {
   VITE_API_URL: 'http://localhost:8000',
